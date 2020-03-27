e0c997449cb4d68536c4d694c3f994703271e25c
sujiayi317 <sujiayi317@gmail.com>
2020-03-27 15:18:35 -0400

    Lecture notes

---
+++ lectures/lab1.java
@@ -0,0 +1,16 @@
+// This is a single-line comment
+
+/* This is a
+   multi-line
+   comment */
+
+/* This is a
+ * multi-line
+ * comment */
+
+/ * Finally, a subclass of the multiline comments called documentation comments or javadoc comments are intended to
+        provide specifications of methods (functions), classes, and instance variables. Our style rules, in fact,
+        require them. For example. */
+
+/** Returns the current size of the list. */
+// public int size() { ... }
