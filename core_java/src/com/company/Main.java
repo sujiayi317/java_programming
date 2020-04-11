547efe4fefc76afa873b51fbe192f91c7575dddb
sujiayi317 <sujiayi317@gmail.com>
2020-04-11 18:24:26 -0400

    start a new java project

---
+++ core_java/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import com.company.model.Product;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* write your code here */
+
+        System.out.println(true);
+
+        Product product = new Product("Keyborad");
+
+        System.out.println(product.getName());
+    }
+}
+
